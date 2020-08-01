eb10235b295612bc469cdad20ecfe9153121215c
Typhon-Divinity <u17058849@tuks.co.za>
2020-08-01 16:07:12 +0200

    Further

--- lightbot_web/src/Layouts/Home.js
+++ lightbot_web/src/Layouts/Home.js
@@ -1,25 +1,78 @@
 // Importing Libraries
 import React from 'react'
-import PerfectScrollbar from "perfect-scrollbar";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Footer from "components/Footer/Footer.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import Navbar from "components/Navbars/Navbar.js";
+import PerfectScrollbar from 'perfect-scrollbar'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Footer from 'components/Footer/Footer.js'
+import Sidebar from 'components/Sidebar/Sidebar.js'
+import Navbar from 'components/Navbars/Navbar.js'
 
+var ps
 
-export default function Home() {
+class Home extends React.Component {
+  state = {
+    backgroundColor: 'blue',
+  }
+  mainPanel = React.createRef()
+  componentDidMount() {
+    if (navigator.platform.indexOf('Win') > -1) {
+      ps = new PerfectScrollbar(this.mainPanel.current)
+      document.body.classList.toggle('perfect-scrollbar-on')
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf('Win') > -1) {
+      ps.destroy()
+      document.body.classList.toggle('perfect-scrollbar-on')
+    }
+  }
+  componentDidUpdate(e) {
+    if (e.history.action === 'PUSH') {
+      document.documentElement.scrollTop = 0
+      document.scrollingElement.scrollTop = 0
+      this.mainPanel.current.scrollTop = 0
+    }
+  }
+  handleColorClick = (color) => {
+    this.setState({ backgroundColor: color })
+  }
+  render() {
     return (
-        <div className='wrapper' style={MyStyles.backgroundStyle}>
+      <div className='wrapper' style={MyStyles.backgroundStyle}>
+        <Sidebar
+          {...this.props}
+          routes={routes}
+          backgroundColor={this.state.backgroundColor}
+        />
+        <div className='main-panel' ref={this.mainPanel}>
+          <Navbar {...this.props} />
+          <Switch>
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.layout + prop.path}
+                  component={prop.component}
+                  key={key}
+                />
+              )
+            })}
+            <Redirect from='/admin' to='/admin/dashboard' />
+          </Switch>
+          <Footer fluid />
         </div>
+      </div>
     )
+  }
 }
 
+export default Home
+
 const MyStyles = {
-    backgroundStyle:{
-        backgroundImage: 'url(' + require('../assets/images/Dash_Background.png') + ')',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        height: '100vh'
-    }
+  backgroundStyle: {
+    backgroundImage:
+      'url(' + require('../assets/images/Dash_Background.png') + ')',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+  },
 }
--- lightbot_web/src/Layouts/Landing.js
+++ lightbot_web/src/Layouts/Landing.js
@@ -1,18 +1,67 @@
 import React from 'react'
 
-export default function Landing() {
+var ps
+
+class Landing extends React.Component {
+  state = {
+    backgroundColor: 'blue',
+  }
+  mainPanel = React.createRef()
+  componentDidMount() {
+    if (navigator.platform.indexOf('Win') > -1) {
+      ps = new PerfectScrollbar(this.mainPanel.current)
+      document.body.classList.toggle('perfect-scrollbar-on')
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf('Win') > -1) {
+      ps.destroy()
+      document.body.classList.toggle('perfect-scrollbar-on')
+    }
+  }
+  componentDidUpdate(e) {
+    if (e.history.action === 'PUSH') {
+      document.documentElement.scrollTop = 0
+      document.scrollingElement.scrollTop = 0
+      this.mainPanel.current.scrollTop = 0
+    }
+  }
+  handleColorClick = (color) => {
+    this.setState({ backgroundColor: color })
+  }
+  render() {
     return (
-        <div className='wrapper' style={MyStyles.backgroundStyle}>
+      <div className='wrapper' style={MyStyles.backgroundStyle}>
+        <div className='main-panel' ref={this.mainPanel}>
+          <Navbar {...this.props} />
+          <Switch>
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.layout + prop.path}
+                  component={prop.component}
+                  key={key}
+                />
+              )
+            })}
+            <Redirect from='/landing' to='/landing/login' />
+          </Switch>
+          <Footer fluid />
         </div>
+      </div>
     )
+  }
 }
 
+export default Landing
+
 const MyStyles = {
-    backgroundStyle:{
-        backgroundImage: 'url(' + require('../assets/images/Home_Background.jpg') + ')',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        height: '100vh'
-    }
+  backgroundStyle: {
+    backgroundImage:
+      'url(' + require('../assets/images/Home_Background.jpg') + ')',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+  },
 }
