974a9c78e52a96e47e1d57dec4b5e41e2d7fac07
Jad-91802 <35114725+Jad-91802@users.noreply.github.com>
2020-08-03 20:13:06 +0200

    Fixed import paths for Home.js and Landing.js and Prettier

--- lightbot_web/src/index.js
+++ lightbot_web/src/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createBrowserHistory } from 'history'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "./assets/scss/now-ui-dashboard.scss?v1.4.0";
-import "./assets/css/now-ui-kit.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/scss/now-ui-dashboard.scss?v1.4.0'
+import './assets/css/now-ui-kit.css'
 
-import Home from "./layouts/Home.js";
-import Landing from "./layouts/Landing.js";
+import Home from './Layouts/Home.js'
+import Landing from './Layouts/Landing.js'
 
-const hist = createBrowserHistory();
+const hist = createBrowserHistory()
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/landing" render={(props) => <Landing {...props} />} />
-      <Route path="/home" render={(props) => <Home {...props} />} />
-      <Redirect to="/404" />
+      <Route path='/landing' render={(props) => <Landing {...props} />} />
+      <Route path='/home' render={(props) => <Home {...props} />} />
+      <Redirect to='/404' />
     </Switch>
   </Router>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
