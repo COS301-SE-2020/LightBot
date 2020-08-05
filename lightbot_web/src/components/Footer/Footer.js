f947132e85f09510b3441a24948db70ff29b9475
Rahul Kapoor <42545849+rahulkap20@users.noreply.github.com>
2020-08-05 20:24:40 +0200

    Updated Footer.js
    Changed disply text

--- lightbot_web/src/components/Footer/Footer.js
+++ lightbot_web/src/components/Footer/Footer.js
@@ -1,68 +1,68 @@
-import React from "react";
-import { Container } from "reactstrap";
-import PropTypes from "prop-types";
+import React from 'react'
+import { Container } from 'reactstrap'
+import PropTypes from 'prop-types'
 
 class Footer extends React.Component {
   render() {
     return (
       <footer
-        className={"footer" + (this.props.default ? " footer-default" : "")}
+        className={'footer' + (this.props.default ? ' footer-default' : '')}
       >
         <Container fluid={this.props.fluid ? true : false}>
           <nav>
             <ul>
               <li>
                 <a
-                  href="https://www.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
+                  href='https://www.creative-tim.com?ref=nudr-footer'
+                  target='_blank'
                 >
                   Creative Tim
                 </a>
               </li>
               <li>
                 <a
-                  href="https://presentation.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
+                  href='https://presentation.creative-tim.com?ref=nudr-footer'
+                  target='_blank'
                 >
                   About Us
                 </a>
               </li>
               <li>
                 <a
-                  href="https://blog.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
+                  href='https://blog.creative-tim.com?ref=nudr-footer'
+                  target='_blank'
                 >
                   Blog
                 </a>
               </li>
             </ul>
           </nav>
-          <div className="copyright">
-            &copy; {1900 + new Date().getYear()}, Coded by{" "}
+          <div className='copyright'>
+            &copy; {1900 + new Date().getYear()}, Brought to you by{' '}
             <a
-              href="https://www.creative-tim.com?ref=nudr-footer"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.creative-tim.com?ref=nudr-footer'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Gradient
             </a>
             .
             <img
-            className='footerLogo'
-            alt='...'
-            src={require('../../assets/img/Gradient_Logo_White.png')}
-            style={{height:'5vh'}}
-          />
+              className='footerLogo'
+              alt='...'
+              src={require('../../assets/img/Gradient_Logo_White.png')}
+              style={{ height: '5vh' }}
+            />
           </div>
         </Container>
       </footer>
-    );
+    )
   }
 }
 
 Footer.propTypes = {
   default: PropTypes.bool,
   fluid: PropTypes.bool,
-};
+}
 
-export default Footer;
+export default Footer
