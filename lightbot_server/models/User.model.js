7e8f4ecf25c02a29f0c124874fe33533a220e7fd
Thiveshan <u15007911@tuks.co.za>
2020-08-14 23:15:26 +0200

    Update User.model.js

--- lightbot_server/models/User.model.js
+++ lightbot_server/models/User.model.js
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: '../../lightbot_web/src/assets/img/default-avatar.jpg',
+    default: '../../assets/img/default-avatar.jpg',
   },
   date: {
     type: Date,
