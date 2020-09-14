ff0ddd3a4645f35e548c55b02015621f3f3581db
u15007911 <u15007911@tuks.co.za>
2020-09-14 15:58:26 +0200

    Update RegisterPage.spec.js
    Do all testing for register page

--- lightbot_web/cypress/integration/lightbot/RegisterPage.spec.js
+++ lightbot_web/cypress/integration/lightbot/RegisterPage.spec.js
@@ -13,177 +13,179 @@ describe('Unit Test our login page', () => {
       cy.visit('/register') // change URL to match your dev URL
     })
 
-    // //Check if link to login page works
-    // it('Test link to login page', () => {
-    //   cy.get('div.pull-left > h6 > .link')
-    //     .click()
-    //     .then(() => {
-    //       cy.url().should('contain', Cypress.config().baseUrl + '/login')
-    //     })
-    // })
+    //Check if link to login page works
+    it('Test link to login page', () => {
+      cy.get('div.pull-left > h6 > .link')
+        .click()
+        .then(() => {
+          cy.url().should('contain', Cypress.config().baseUrl + '/login')
+        })
+    })
 
-    // //Check if link to recovery page works
-    // it('Test link to recovery page', () => {
-    //   cy.get('div.pull-right > h6 > .link')
-    //     .click()
-    //     .then(() => {
-    //       cy.url().should('contain', Cypress.config().baseUrl + '/recovery')
-    //     })
-    // })
+    //Check if link to recovery page works
+    it('Test link to recovery page', () => {
+      cy.get('div.pull-right > h6 > .link')
+        .click()
+        .then(() => {
+          cy.url().should('contain', Cypress.config().baseUrl + '/recovery')
+        })
+    })
 
-    // //Check if able to type data into all fields
-    // it('Type into allfields', () => {
-    //   // Test Email
-    //   cy.get('#idName')
-    //     .clear()
-    //     .type('Cypress')
-    //     .should('have.value', 'Cypress')
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('Test')
-    //     .should('have.value', 'Test')
-    //     .get('#idEmail')
-    //     .clear()
-    //     .type('fake@email.com')
-    //     .should('have.value', 'fake@email.com')
-    //     //Test password field1
-    //     .get('#idPassword')
-    //     .type('Password1@')
-    //     .should('have.value', 'Password1@')
-    //     //Test password field2
-    //     .get('#idPassword2')
-    //     .type('Password1@')
-    //     .should('have.value', 'Password1@')
-    // })
+    //Check if able to type data into all fields
+    it('Type into allfields', () => {
+      // Test name field
+      cy.get('#idName')
+        .clear()
+        .type('Cypress')
+        .should('have.value', 'Cypress')
+        //Test surname field
+        .get('#idSurname')
+        .clear()
+        .type('Test')
+        .should('have.value', 'Test')
+        //Test email field
+        .get('#idEmail')
+        .clear()
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
+        //Test password field1
+        .get('#idPassword')
+        .type('Password1@')
+        .should('have.value', 'Password1@')
+        //Test password field2
+        .get('#idPassword2')
+        .type('Password1@')
+        .should('have.value', 'Password1@')
+    })
 
-    // //Check input validation for all fields
-    // it('Input validation for all fields', () => {
-    //   //Input validation for Name
-    //   cy.get('#idName')
-    //     .clear()
-    //     .type('Ab')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idName')
-    //     .clear()
-    //     .type('123')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idName')
-    //     .clear()
-    //     .type('Ab32')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idName')
-    //     .clear()
-    //     .type('Test')
-    //     .get('div.invalid-feedback')
-    //     .should('not.be.visible')
-    //     //Input validation for Surname
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('Ab')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('123')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('Ab32')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('Test')
-    //     .get('div.invalid-feedback')
-    //     .should('not.be.visible')
-    //     //Input validation for Email
-    //     .get('#idEmail')
-    //     .clear()
-    //     .type('fake@email')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idEmail')
-    //     .clear()
-    //     .type('fake@email.com')
-    //     .get('div.invalid-feedback')
-    //     .should('not.be.visible')
-    //     //Input validation for password1
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type('password')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type('password1')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type('password@')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type('Password1@')
-    //     .get('div.invalid-feedback')
-    //     .should('not.be.visible')
-    //     //Input validation for password2
-    //     .get('#idPassword2')
-    //     .clear()
-    //     .type('password')
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    //     .get('#idPassword2')
-    //     .clear()
-    //     .type('Password1@')
-    //     .get('div.invalid-feedback')
-    //     .should('not.be.visible')
-    //     .get('#idPassword2')
-    //     .clear()
-    //     .get('div.invalid-feedback')
-    //     .should('be.visible')
-    // })
+    //Check input validation for all fields
+    it('Input validation for all fields', () => {
+      //Input validation for Name
+      cy.get('#idName')
+        .clear()
+        .type('Ab')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idName')
+        .clear()
+        .type('123')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idName')
+        .clear()
+        .type('Ab32')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idName')
+        .clear()
+        .type('Test')
+        .get('div.invalid-feedback')
+        .should('not.be.visible')
+        //Input validation for Surname
+        .get('#idSurname')
+        .clear()
+        .type('Ab')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idSurname')
+        .clear()
+        .type('123')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idSurname')
+        .clear()
+        .type('Ab32')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idSurname')
+        .clear()
+        .type('Test')
+        .get('div.invalid-feedback')
+        .should('not.be.visible')
+        //Input validation for Email
+        .get('#idEmail')
+        .clear()
+        .type('fake@email')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idEmail')
+        .clear()
+        .type('fake@email.com')
+        .get('div.invalid-feedback')
+        .should('not.be.visible')
+        //Input validation for Password
+        .get('#idPassword')
+        .clear()
+        .type('password')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idPassword')
+        .clear()
+        .type('password1')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idPassword')
+        .clear()
+        .type('password@')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idPassword')
+        .clear()
+        .type('Password1@')
+        .get('div.invalid-feedback')
+        .should('not.be.visible')
+        //Input validation for Confirm Password
+        .get('#idPassword2')
+        .clear()
+        .type('password')
+        .get('div.invalid-feedback')
+        .should('be.visible')
+        .get('#idPassword2')
+        .clear()
+        .type('Password1@')
+        .get('div.invalid-feedback')
+        .should('not.be.visible')
+        .get('#idPassword2')
+        .clear()
+        .get('div.invalid-feedback')
+        .should('be.visible')
+    })
 
-    // //Test register functionality with existing email
-    // it('Test incorrect email request', () => {
-    //   cy.server()
-    //   cy.route({
-    //     method: 'POST',
-    //     url: '/user/register',
-    //   }).as('apiCheck')
-    //   cy.get('#idName')
-    //     .clear()
-    //     .type('Name')
-    //     .get('#idSurname')
-    //     .clear()
-    //     .type('Surname')
-    //     .get('#idEmail')
-    //     .clear()
-    //     .type(userEmail)
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type('Password1@')
-    //     .get('#idPassword2')
-    //     .clear()
-    //     .type('Password1@')
-    //     .get('.btn-round')
-    //     .contains('Register')
-    //     .click()
-    //   cy.wait('@apiCheck').then((xhr) => {
-    //     assert.equal(
-    //       xhr.response.body.error.message,
-    //       'User already exists please sign in.'
-    //     )
-    //   })
-    // })
+    //Test register functionality with existing email
+    it('Test register with existing email', () => {
+      cy.server()
+      cy.route({
+        method: 'POST',
+        url: '/user/register',
+      }).as('apiCheck')
+      cy.get('#idName')
+        .clear()
+        .type('Name')
+        .get('#idSurname')
+        .clear()
+        .type('Surname')
+        .get('#idEmail')
+        .clear()
+        .type(userEmail)
+        .get('#idPassword')
+        .clear()
+        .type('Password1@')
+        .get('#idPassword2')
+        .clear()
+        .type('Password1@')
+        .get('.btn-round')
+        .contains('Register')
+        .click()
+      cy.wait('@apiCheck').then((xhr) => {
+        assert.equal(
+          xhr.response.body.error.message,
+          'User already exists please sign in.'
+        )
+      })
+    })
 
     //Test valid register request and check that user is redirected to login page
-    it('Test valid login request', () => {
+    it('Test valid reigster request', () => {
       cy.server()
       cy.route({
         method: 'POST',
@@ -207,54 +209,21 @@ describe('Unit Test our login page', () => {
         .get('.btn-round')
         .contains('Register')
         .click()
-      cy.wait('@apiCheck').then((xhr) => {
-        assert.equal(
-          xhr.response.body.error.message,
-          'User already exists please sign in.'
-        )
-        cy.url().should('contain', Cypress.config().baseUrl)
+      cy.wait('@apiCheck')
+        .then((xhr) => {
+          assert.equal(xhr.status, '200')
+        })
+        //Check if redirected to login page
+        .then(() => {
+          cy.url().should('contain', Cypress.config().baseUrl + '/login')
+        })
+      //Check new user can be logged in
+      cy.request('GET', '/user/login', {
+        User_email: 'lightbot_cypress@testingb.web',
+        User_password: 'Cypress301#',
+      }).then((xhr) => {
+        assert.equal(xhr.status, '200')
       })
     })
-
-    // //Test if cookie works
-    // it('Test to see if valid login loads Cookie, and tests functionality of Cookie', () => {
-    //   cy.server()
-    //   cy.route({
-    //     method: 'POST',
-    //     url: '/user/login',
-    //   }).as('apiLogin')
-    //   cy.get('#idEmail')
-    //     .clear()
-    //     .type(userEmail)
-    //     .get('#idPassword')
-    //     .clear()
-    //     .type(userPass)
-    //     .get('.btn-round')
-    //     .contains('Login')
-    //     .click()
-    //   cy.wait('@apiLogin').then(() => {
-    //     //Check if cookie exits
-    //     cy.getCookie('token').should('exist')
-    //   })
-    //   //Check if cookie works
-    //   cy.server()
-    //   cy.route({
-    //     method: 'GET',
-    //     url: '/user/me',
-    //   }).as('apiMe')
-    //   cy.visit('/')
-    //   //If token exits and visit login page should be redirected to overview page
-    //   cy.wait('@apiMe')
-    //     .then((xhr) => {
-    //       //Check to see if correct profile loaded
-    //       assert.equal(xhr.response.body.success.data.User_email, userEmail)
-    //     })
-    //     .then(() => {
-    //       cy.url().should(
-    //         'contain',
-    //         Cypress.config().baseUrl + '/home/overview'
-    //       )
-    //     })
-    // })
   })
 })
