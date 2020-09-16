72bc567864d661d56d961f53797548a80532eb0d
Rahul Kapoor <42545849+rahulkap20@users.noreply.github.com>
2020-09-17 00:55:57 +0200

    Update MongoConnection.py
    Added doxygen comments.

--- lightbot_ai/Adaptive_Traffic_Simulation/Common/MongoConnection.py
+++ lightbot_ai/Adaptive_Traffic_Simulation/Common/MongoConnection.py
@@ -1,3 +1,4 @@
+# This file represents a connection to the MongoDB database for the system.
 import pymongo
 
 
@@ -6,6 +7,8 @@ class MongoDBConnect:
 	conn = None
 	coll = None
 	
+	# The constructor, which stores the parameters into their respective member variable.
+	#  @param self The object pointer.
 	def __init__(self):
 		try:
 			self.conn = pymongo.MongoClient("mongodb+srv://LightBotAdmin:lightbot@lightbot-8xen0.mongodb.net/LightBot?retryWrites=true&w=majority")
@@ -14,17 +17,28 @@ class MongoDBConnect:
 		except pymongo.errors.ConnectionFailure:
 			print ("Could not connect to DB")
 	
+	# The destructor.
+	#  @param self The object pointer.
 	def __del__(self): 
 		if self.conn is not None:
 			self.coll = None
 			self.conn.close()
 	
+	# Documentation for the addOneToDB method.
+	#  @param self The object pointer.
+	#  @param dictArg The data to be added to the database represented as a dictonary.
 	def addOneToDB(self, dictArg):
 		x = self.coll.insert_one(dictArg)
 
+	# Documentation for the addManyToDB method.
+	#  @param self The object pointer.
+	#  @param listArg The data to be added to the database represented as a list.
 	def addManyToDB(self, listArg):
 		x = self.coll.insert_many(listArg)
 
+	# Documentation for the appendAvgLength method.
+	#  @param self The object pointer.
+	#  @param avg The average queue length to be added to the database.
 	def appendAvgLength(self, avg):
 		x = self.coll.find_one({},{ "_id": 0 , "avgQueueLengths": 1})
 		if x is None: 
@@ -36,6 +50,9 @@ class MongoDBConnect:
 			lengths.append(avg)
 			self.coll.update_one(x, {"$set":{"avgQueueLengths" : lengths}})
 
+	# Documentation for the appendTotalWaitTime method.
+	#  @param self The object pointer.
+	#  @param wait The total waiting time to be added to the database.
 	def appendTotalWaitTime(self, wait):
 		x = self.coll.find_one({},{ "_id": 0 , "totalWaitTime": 1})
 		if x is None: 
@@ -47,6 +64,9 @@ class MongoDBConnect:
 			times.append(wait)
 			self.coll.update_one(x, {"$set":{"totalWaitTime" : times}})
 
+	# Documentation for the addTotalQueueLength method.
+	#  @param self The object pointer.
+	#  @param queueList The the total queue length to be added to the database.
 	def addTotalQueueLength(self, queueList):
 		x = self.coll.find_one({},{ "_id": 0 , "totalQueueLegth": 1})		
 		if x is None: 
@@ -54,6 +74,8 @@ class MongoDBConnect:
 		else:
 			self.coll.update_one(x, {"$set":{"totalQueueLegth" : queueList}})
 
+	# Documentation for the queryAllFromDB method.
+	#  @param self The object pointer.
 	def queryAllFromDB(self):
 		results = self.coll.find({},{ "_id": 0 })
 		out = []
@@ -61,9 +83,13 @@ class MongoDBConnect:
 			out.append(x)
 		return out
 		
+	# Documentation for the getStoredState method.
+	#  @param self The object pointer.
 	def getStoredState(self):
 		result = self.coll.find_one({ "Title": "StoredState" },{ "_id": 0 })
 		return result
 		
+	# Documentation for the deleteStoredState method.
+	#  @param self The object pointer.
 	def deleteStoredState(self):
 		self.coll.delete_one({ "Title": "StoredState" })
