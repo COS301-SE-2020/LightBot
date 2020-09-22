696faf0319f450cfceb66ec2cf063abfbc17c3a9
Typhon-Divinity <u17058849@tuks.co.za>
2020-09-22 22:07:33 +0200

    Update api.js

--- lightbot_web/src/services/api.js
+++ lightbot_web/src/services/api.js
@@ -3,8 +3,8 @@ import { LOGOUT } from '../actions/types'
 import store from '../store'
 
 const api = axios.create({
-  // baseURL: 'https://api.lightbot.co.za:8000/',
-  baseURL: 'http://ec2-18-192-23-11.eu-central-1.compute.amazonaws.com:8000/',
+  baseURL: 'https://api.lightbot.co.za/',
+  //baseURL: 'http://ec2-18-192-23-11.eu-central-1.compute.amazonaws.com:8000/',
   //baseURL: 'http://localhost:8000/',
   headers: {
     'Content-Type': 'application/json',
