0de3557884c1a651fc546fce104f184645a32bee
Typhon-Divinity <u17058849@tuks.co.za>
2020-09-22 22:36:51 +0200

    amplify init

--- lightbot_web/src/index.js
+++ lightbot_web/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
